6a8ab8a8771f0026bc27b02b64df63cf59b8eb1d
Manuel Ernesto Bonilla Muñoz <mebonilla9@gmail.com>
2017-06-27 16:47:44 -0500

    Creacion de actividad login

---
+++ app/src/main/java/co/appreactor/proyectofull/negocio/LoginActivity.java
@@ -0,0 +1,15 @@
+package co.appreactor.proyectofull.negocio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import co.appreactor.proyectofull.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
